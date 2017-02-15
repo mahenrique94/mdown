fea36a905669d208bab6d6fb5e23e3f73d8839f9
Matheus <matheushcastiglioni@gmail.com>
2017-02-15 20:41:06 -0200

    Refatorado a forma que estava realizando o markdown

--- mdown.js
+++ mdown.js
@@ -1,47 +1,42 @@
 /** @auth Matheus Castiglioni
- *  Expressões regulares para realizar o mark down 
+ *  Editor que faz uso do markdown
  */
-var EDITOR_CURSOR_POSITION_BEGIN, EDITOR_CURSOR_POSITION_END;
+const REGEX_P = new RegExp('^(.+)$', 'gim');
+const REGEX_STRONG = new RegExp('(([*]{2})(.+)([*]{2}))', 'gim');
+const REGEX_EM = new RegExp('(([_])(.+)([_]))', 'gim');
+const REGEX_H1 = new RegExp('(([#]\\s)([^\\<]+))', 'gim');
+const REGEX_H2 = new RegExp('(([#]{2}\\s)([^\\<]+))', 'gim');
+const REGEX_H3 = new RegExp('(([#]{3}\\s)([^\\<]+))', 'gim');
+const REGEX_H4 = new RegExp('(([#]{4}\\s)([^\\<]+))', 'gim');
+const REGEX_H5 = new RegExp('(([#]{5}\\s)([^\\<]+))', 'gim');
+const REGEX_H6 = new RegExp('(([#]{6}\\s)([^\\<]+))', 'gim');
+const REGEX_A = new RegExp('(([\\[])(.+)([\\]])([(])(.+)([)]))', 'gim');
+const REGEX_IMG = new RegExp('(([!])([\\[])(.+)([\\]])([(])(.+)([)]))', 'gim');
+const REGEX_IFRAME = new RegExp('(([?])([\\[])(.+)([\\]])([(])(.+)([)]))', 'gim');
+const REGEX_BLOCKQUOTE = new RegExp('(([\\>]\\s)([^\\<]+))', 'gim');
+const REGEX_CODE = new RegExp('(([`]{3})([A-Z\\-]*)([\\w\\W\\s\\S\\n\\r\\d\\D]+)([`]{3}))', 'gim');
 
-const INDEX_BEGIN_END = 3;
-const INDEX_CONTENT = 2;
-const INDEX_MARKDOWN = 0;
-const INDEX_REGEX = 0;
-const INDEX_TAG = 1;
-
-                 // [EXPRESSAO_REGULAR, TAG_PARA_SER_ADICIONADO, INDICE_DO_GRUPO_QUE_VAI_DENTRO_DA_TAG, INFORMAR ^ e $ ?]
-const REGEXP_P = [new RegExp('^(.+)$', 'gim'), 'p', 0, true]; // <p></p>
-const REGEXP_H1 = [new RegExp('^(([#])(.*))$', 'gim'), 'h1', 3, false]; //<h1></h1>
-const REGEXP_H2 = [new RegExp('^(([#]{2})(.*))$', 'gim'), 'h2', 3, false]; //<h2></h2>
-const REGEXP_H3 = [new RegExp('^(([#]{3})(.*))$', 'gim'), 'h3', 3, false]; //<h3></h3>
-const REGEXP_H4 = [new RegExp('^(([#]{4})(.*))$', 'gim'), 'h4', 3, false]; //<h4></h4>
-const REGEXP_H5 = [new RegExp('^(([#]{5})(.*))$', 'gim'), 'h5', 3, false]; //<h5></h5>
-const REGEXP_H6 = [new RegExp('^(([#]{6})(.*))$', 'gim'), 'h6', 3, false]; //<h6></h6>
-const REGEXP_STRONG = [new RegExp('(([*]{2})(.*)([*]{2}))', 'gim'), 'strong', 3, false]; //<strong></strong>
-const REGEXP_I = [new RegExp('(([_])(.*)([_]))', 'gim'), 'em', 3, false]; //<em></em>
-const REGEXP_A = [new RegExp('((([\\[])(.*)([\\]])(([(])(.*)([)]))))', 'gim'), ['a', 'href'], [4, 8], false]; // <a></a>
-const REGEXP_IMG = [new RegExp('(([!])(([\\[])(.*)([\\]])(([(])(.*)([)]))))', 'gim'), ['img', 'src', 'alt'], [5, 9, 5], false]; // <img></img>
-const REGEXP_VIDEO = [new RegExp('(([?])(([\\[])(.*)([\\]])(([(])(.*)([)]))))', 'gim'), ['iframe', 'src'], [5, 9], false]; // <iframe></iframe>
-const REGEXP_BLOCKQUOTE = [new RegExp('^(([>])(.*))$', 'gim'), 'blockquote', 3, false]; //<blockquote></blockquote>
-const REGEXP_UL = [new RegExp('^(([-])(.*))$', 'gim'), 'li', 3, false]; //<li></li>
-const REGEXP_OL = [new RegExp('^(([\\d][.])(.*))$', 'gim'), 'li', 3, false]; //<ol></ol>
-const REGEXS = [
-    REGEXP_BLOCKQUOTE,
-    REGEXP_UL,
-    REGEXP_OL,
-    REGEXP_H6,
-    REGEXP_H5,
-    REGEXP_H4,
-    REGEXP_H3,
-    REGEXP_H2,
-    REGEXP_H1,
-    REGEXP_STRONG,
-    REGEXP_I,
-    REGEXP_IMG,
-    REGEXP_VIDEO,
-    REGEXP_A,
-    REGEXP_P
-];
+/** @auth Matheus Castiglioni
+ *  Processa cada tag markdown
+ */
+function processMarkDown(editor) {
+    let html = editor.value;
+    html = markDownP(html);
+    html = markDownStrong(html);
+    html = markDownEM(html);
+    html = markDownH6(html);
+    html = markDownH5(html);
+    html = markDownH4(html);
+    html = markDownH3(html);
+    html = markDownH2(html);
+    html = markDownH1(html);
+    html = markDownIMG(html);
+    html = markDownIframe(html);
+    html = markDownA(html);
+    html = markDownBlockquote(html);
+    html = markDownCode(html);
+    insertOutput(editor, html);
+}
 
 /** @auth Matheus Castiglioni
  *  Função para pegar a posição do cursor no textarea e inserir os markdowns
@@ -54,58 +49,109 @@ document.querySelectorAll('.md-editor__data').forEach(editor => {
 });
 
 /** @auth Matheus Castiglioni
- *  Processa a formatação a cada tecla digitada no textarea
+ *  Processa o markdown da tag P
  */
-function processMarkDown(textarea) {
-    let output = textarea.parentNode.querySelector('output');
-    let html = processEachRegex(textarea);
+function markDownP(html) {
+    return html.replace(REGEX_P, '<p>$1</p>');
+}
+
+/** @auth Matheus Castiglioni
+ *  Processa o markdown da tag STRONG
+ */
+function markDownStrong(html) {
+    return html.replace(REGEX_STRONG, '<strong>$3</strong>');
+}
+
+/** @auth Matheus Castiglioni
+ *  Processa o markdown da tag EM
+ */
+function markDownEM(html) {
+    return html.replace(REGEX_EM, '<em>$3</em>');
+}
+
+/** @auth Matheus Castiglioni
+ *  Processa o markdown da tag H1
+ */
+function markDownH1(html) {
+    return html.replace(REGEX_H1, '<h1>$3</h1>');
+}
+
+/** @auth Matheus Castiglioni
+ *  Processa o markdown da tag H2
+ */
+function markDownH2(html) {
+    return html.replace(REGEX_H2, '<h2>$3</h2>');
+}
+
+/** @auth Matheus Castiglioni
+ *  Processa o markdown da tag H3
+ */
+function markDownH3(html) {
+    return html.replace(REGEX_H3, '<h3>$3</h3>');
+}
+
+/** @auth Matheus Castiglioni
+ *  Processa o markdown da tag H4
+ */
+function markDownH4(html) {
+    return html.replace(REGEX_H4, '<h4>$3</h4>');
+}
+
+/** @auth Matheus Castiglioni
+ *  Processa o markdown da tag H5
+ */
+function markDownH5(html) {
+    return html.replace(REGEX_H5, '<h5>$3</h5>');
+}
+
+/** @auth Matheus Castiglioni
+ *  Processa o markdown da tag H6
+ */
+function markDownH6(html) {
+    return html.replace(REGEX_H6, '<h6>$3</h6>');
+}
+
+/** @auth Matheus Castiglioni
+ *  Processa o markdown da tag A
+ */
+function markDownA(html) {
+    return html.replace(REGEX_A, '<a href="$6">$3</a>');
+}
+
+/** @auth Matheus Castiglioni
+ *  Processa o markdown da tag IMG
+ */
+function markDownIMG(html) {
+    return html.replace(REGEX_IMG, '<img alt="$4" src="$7">');
+}
+
+/** @auth Matheus Castiglioni
+ *  Processa o markdown da tag IFRAME
+ */
+function markDownIframe(html) {
+    return html.replace(REGEX_IFRAME, '<iframe src="$7">');
+}
+
+/** @auth Matheus Castiglioni
+ *  Processa o markdown da tag BLOCKQUOTE
+ */
+function markDownBlockquote(html) {
+    return html.replace(REGEX_BLOCKQUOTE, '<blockquote>$3</blockquote>');
+}
+
+/** @auth Matheus Castiglioni
+ *  Processa o markdown da tag code inline
+ */
+function markDownCode(html) {
+    return html.replace(REGEX_CODE, '<pre class="language-xxxx $3"><code class="language-xxxx $3">$4</code></pre>');
+}
+
+/** @auth Matheus Castiglioni
+ *  Pega o html gerado pelo markdown e insere no preview
+ */
+function insertOutput(editor, html) {
+    let output = editor.nextSibling.nextSibling;
     output.innerHTML = html;
-}
-
-/** @auth Matheus Castiglioni
- *  Processa cada expressão regular e troca para sua determinada tag HTML 
- */
-function processEachRegex(textarea) {
-    let html = textarea.value;
-    let match;
-    for(let i = 0; i < REGEXS.length; i++) {
-        while(match = REGEXS[i][INDEX_REGEX].exec(textarea.value)) {            
-            html = replaceMarkDownForTag(html, match, REGEXS[i]);
-        }
-    }
-    return html;
-}
-
-/** @auth Matheus Castiglioni
- *  Troca o valor do mark down pela tag
- */
-function replaceMarkDownForTag(value, match, regexp) {    
-    let regExpMatch = buildRegExpMatch(regexp[INDEX_BEGIN_END], match[INDEX_MARKDOWN]);
-    let html = value.replace(regExpMatch, buildTag(match, regexp[INDEX_CONTENT], regexp[INDEX_TAG]));
-    return html;
-}
-
-/** @auth Matheus Castiglioni
- *  Criar expressão para trocar a tag apenas no pedaço que a mesma foi encontrada e não procurar no 
- *  value do campo inteiro
- */
-function buildRegExpMatch(beginEnd, match) {
-    let regExp = new RegExp(`^([${match.trim().replace(/[\[]/g, '\\[').replace(/[\]]/g, '\\]').replace(/[\/]/g, '\\/').replace(/[\-]/g, '\\-')}]{${match.trim().length}})$`);
-    if (!beginEnd)
-        regExp = new RegExp(`([${match.replace(/[\[]/g, '\\[').replace(/[\]]/g, '\\]').replace(/[\/]/g, '\\/').replace(/[\-]/g, '\\-')}]{${match.length}})`);
-    return regExp;
-}
-
-/** @auth Matheus Castiglioni
- *  Pega o valor encontrado de acordo com cada regex e o devolve entre as TAG's
- */
-function buildTag(match, content, tag) {
-    let html;
-    if (Array.isArray(tag) && Array.isArray(content))
-        html = `<${tag[0]} ${tag[1]}="${match[content[1]]}">${match[content[0]].trim()}</${tag[0]}>`;
-    else
-        html = `<${tag}>${match[content].trim()}</${tag}>`;
-    return html;
 }
 
 /** @auth Matheus Castiglioni
@@ -265,6 +311,15 @@ function insertListOrdered(button) {
     const editor = button.parentNode.parentNode.parentNode.parentNode.querySelector('.md-editor__data');
     if (editor)
         insertMarkDown(editor, '1. ', null, EDITOR_CURSOR_POSITION_BEGIN, EDITOR_CURSOR_POSITION_END, 3);
+}
+
+/** @auth Matheus Castiglioni
+ *  Inserir marcação referente a lista ordenada
+ */
+function insertCode(button) {
+    const editor = button.parentNode.parentNode.parentNode.parentNode.querySelector('.md-editor__data');
+    if (editor)
+        insertMarkDown(editor, '```\ninsira seu código aqui\n', '```', EDITOR_CURSOR_POSITION_BEGIN, EDITOR_CURSOR_POSITION_END, 26);
 }
 
 /** @auth Matheus Castiglioni
